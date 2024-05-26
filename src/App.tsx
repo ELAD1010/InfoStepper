2ae0d4da0b1a71524b8e8a17e05937f656d13968
ELAD1010 <elad2911@gmail.com>
2024-05-26 18:25:27 +0300

    add button which opens modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 
+import "./App.css";
+
+import "./components/UserInfoModal";
+import UserInfoModal from "./components/UserInfoModal";
+
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <>
-      <Button>Open Modal</Button>
+      <div className="layout">
+        <Button variant="contained" onClick={handleOpen}>
+          Open Modal
+        </Button>
+        <UserInfoModal open={open} handleClose={handleClose} />
+      </div>
     </>
   );
 }
